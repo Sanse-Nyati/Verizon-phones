478e8a6783ec9a7952924cce8809af3709afb04f
Andrew <afandeandrew639@gmail.com>
2025-05-04 01:22:07 -0400

    Update Chatbot.jsx

--- src/Components/Chatbot.jsx
+++ src/Components/Chatbot.jsx
@@ -15,27 +15,31 @@ const chatbotResponses = [
   },
   {
     regex: /.*Iphone/i,
-    responses: ["It is currently available at a cheaper price Kes: 154000"],
+    responses: ["It is currently available from a lower price of Kes: 154000 only at Verizon Phones.How may i help you with do you want it ordered and Delivered?"],
   },
   {
     regex: /.*Tecno/i,
-    responses: ["Tecno is available at a cheaper price Kes: 15400"],
+    responses: ["Tecno comes at a lower price Kes: 15400 which can be afforded by anyone. How may i help you with do you want it ordered and Delivered?"],
   },
   {
     regex: /.*Samsung/i,
-    responses: ["The price of Samsung is Kes: 23400"],
+    responses: ["The price of Samsung starts at Kes: 23400 upto to the max with diffrent varieties of phones.How may i help you with do you want it ordered, Delivered and paid for?"],
   },
   {
     regex: /.*Oppo/i,
-    responses: ["The price of Oppo is Kes: 164000"],
+    responses: ["Starting from the latest version of Oppo all varieties of Oppo are available.Do you want me to help you with how it will be ordered and delivered."],
   },
   {
     regex: /.*Infinx/i,
-    responses: ["The price of Infinix is Kes: 15000"],
+    responses: ["Starting with the cheaper versions of Infinix all versions are availble just for you. Do you want my assistance how it will be ordered and delivered?"],
   },
   {
     regex: /.*Google Pixel/i,
-    responses: ["The price of Google Pixel is Kes: 256000"],
+    responses: ["The price of Google Pixel is Kes: 25600 starting with the older versions. Do you want my assistance?"],
+  },
+  {
+    regex: /.*yeah|yes.*/i,
+    responses: ["First add your items to the cart, the navigate to your cart using the navbar and then press the blue button "Buy Now" then a pop-up will appear which you will enter your phone number and then a completion pop-up will be sent to your phone for the payment thereafter you willbe given a code that you will use to track your Order by pressing the blue button."],
   },
   {
     regex: /.*/,
@@ -46,7 +50,7 @@ const chatbotResponses = [
 const Chatbot = () => {
   const [userInput, setUserInput] = useState("");
   const [messages, setMessages] = useState([
-    { sender: "chatbot", text: "Hi! I am your Verizon Phones chatbot." },
+    { sender: "chatbot", text: "Hi there! I am your Verizon Phones Assistant." },
   ]);
 
   const handleUserInput = (event) => {
